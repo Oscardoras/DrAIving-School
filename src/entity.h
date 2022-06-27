3c720d19153093d529885148c2793a94b25b8f93
maerhard <mathieu.erhard@etu.uca.fr>
2022-06-27 11:27:22 +0200

    Functions new/free player/entity

--- src/entity.h
+++ src/entity.h
@@ -31,7 +31,7 @@ typedef struct {
  * @param state the Markov state of the entity.
  * @return the new entity.
  */
-Entity* new_entity(Location location, Matrix* markov, State state);
+Entity* new_entity(Location location, Matrix* markov, unsigned int state, unsigned int type);
 
 /**
  * @brief Frees an entity.
