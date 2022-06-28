697c24bcaf5fdd2684580472fd1f930989a33fd1
pachapuis <pachapuis@etu.uca.fr>
2022-06-28 15:30:13 +0200

    main

--- src/main.c
+++ src/main.c
@@ -9,17 +9,20 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+//#define LEARN
+#define LEARN_ITERATION 500
 
 int main() {
     Level* level = new_level(15., 100.);
     if (level == NULL) return EXIT_FAILURE;
     
+    #ifndef LEARN
     Viewport* viewport = create_viewport(WIDTH, HEIGHT, level);
     if (viewport == NULL) {
         free_level(level);
         return EXIT_FAILURE;
     }
-    
+    #endif
     Location player_pos;
     player_pos.y = level->width * 3./5.;
     player_pos.x = 0;
@@ -28,9 +31,17 @@ int main() {
     
     level->player = new_entity(PLAYER_CAR, player_pos, NULL);
     
+    #ifndef LEARN
     event_loop(viewport);
     
     close_viewport(viewport);
+    #else
+    for(unsigned int it = 0; it < LEARN_ITERATION; ++it)
+    {
+
+    }
+    save_matrix(level->player->markov, "learning");
+    #endif
     free_level(level);
 
     return EXIT_SUCCESS;
